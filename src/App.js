88288f1db76eb69a85ed789096d91f1a10e488f9
ClintonYounge <clintonyounge18@gmail.com>
2023-05-04 17:13:15 -0400

    Add styled navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Calculate from './components/calculator';
 import DisplayQuote from './components/DisplayQuote';
+import Home from './components/Home';
+import NavBar from './NavBar';
 
 function App() {
   return (
     <>
-      <DisplayQuote />
-      <Calculate />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculate" element={<Calculate />} />
+        <Route path="/quote" element={<DisplayQuote />} />
+      </Routes>
     </>
   );
 }
